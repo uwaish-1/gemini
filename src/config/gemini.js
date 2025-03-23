01e95c812156cd41039f006d8e2e7857a4c888d0
uwaish-1 <uwaishm82@gmail.com>
2025-03-24 01:48:11 +0530

    updated on 3/24/2025

---
+++ src/config/gemini.js
@@ -0,0 +1,39 @@
+
+
+
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  const apiKey = "AIzaSyDCGsuymQYI1WjNfdp5B2wWFJJByp9sd5k";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-2.0-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    const response=result.response;
+    console.log(result.response.text());
+    return response.text();
+  }
+  
+  export default run;
